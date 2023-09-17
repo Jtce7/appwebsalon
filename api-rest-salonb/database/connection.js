36a362d5c2685c54a7c71b510d40459fd66617e6
Juan <139812735+Jtce7@users.noreply.github.com>
2023-09-17 16:36:49 +0000

    04-setup-mongodb

--- api-rest-salonb/database/connection.js
+++ api-rest-salonb/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connection = async() => {
 
-    try{
+    try {
         await mongoose.connect("mongodb://localhost:27017/appwebsalon");
         
         console.log("Conectado correctamente a bd: appwebsalon");
--- api-rest-salonb/models/user.js
+++ api-rest-salonb/models/user.js
@@ -48,4 +48,4 @@ const UserSchema = new Schema({
 
 });
 
-module.exports = model("User", UserSchema)
+module.exports = model("User", UserSchema, "users")
