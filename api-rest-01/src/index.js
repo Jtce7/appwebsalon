71b5a4e37eb8e6a66ae576d2b403e28b63905afe
Juan <139812735+Jtce7@users.noreply.github.com>
2023-09-22 15:36:13 +0000

    01-Crud

---
+++ api-rest-01/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+//setting
+app.set('port', process.env.PORT || 3900);
+app.set('json space', 2);
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+app.use('/api/movies', require('./routes/movies'));
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
